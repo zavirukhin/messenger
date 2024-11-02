a0548c4555267dba7363a1c8a2d712e0f052118c
HowWow <yarohsyk10@gmail.com>
2024-11-02 22:28:53 +0400

    Init project linter and prettier

--- prettier.config.js
+++
@@ -1,5 +0,0 @@
-export default {
-    singleQuote: true, // Использование одинарных кавычек
-    tabWidth: 4, // Размер табуляции: 4 пробела
-    bracketSpacing: true, // Пробелы между фигурными скобками в объектах
-};
