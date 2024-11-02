789b7cdead40afe083a713e7c9360386c7e04c3d
HowWow <yarohsyk10@gmail.com>
2024-11-02 19:03:01 +0400

    Init project settings

---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+export default {
+    singleQuote: true, // Использование одинарных кавычек
+    tabWidth: 4, // Размер табуляции: 4 пробела
+    bracketSpacing: true, // Пробелы между фигурными скобками в объектах
+};
