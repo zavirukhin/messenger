214e51d2437d0b91730c7ed0ea027a8add77bfe6
alex <aleksandra12mail@mail.ru>
2024-11-05 00:25:44 +0800

    feat: add stylelint, command eslint, prettier, stylelint

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,6 +9,6 @@ export const appConfig: ApplicationConfig = {
     provideAnimations(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
-    NG_EVENT_PLUGINS
+    NG_EVENT_PLUGINS,
   ],
 };
