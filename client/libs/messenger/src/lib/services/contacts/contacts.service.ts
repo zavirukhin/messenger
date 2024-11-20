05ba11cdc98a990241635c7ad4f880c1510a99b6
alex <aleksandra12mail@mail.ru>
2024-11-20 11:35:27 +0800

    fix: contacts

--- client/libs/messenger/src/lib/services/contacts/contacts.service.ts
+++ client/libs/messenger/src/lib/services/contacts/contacts.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { ProfileResponse } from '../../interfaces/profile-response.interface';
 import { environment } from '@env';
 
@@ -10,17 +10,12 @@ import { environment } from '@env';
 export class ContactsService {
   private readonly http = inject(HttpClient);
 
-  private contacts$ = new BehaviorSubject<ProfileResponse[] | null>(null);
 
   public getContacts$(): Observable<ProfileResponse[]> {  
     return this.http
       .get<ProfileResponse[]>(environment.apiUrl + '/contacts/list')
       .pipe(
-        tap((response) => {
-          this.contacts$.next(response);
-
-          return this.contacts$;
-        })
+        tap()
       );
   }
 }
