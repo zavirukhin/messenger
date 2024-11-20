571b7659ce569d443b6152238a4ef1d5870e82ce
alex <aleksandra12mail@mail.ru>
2024-11-20 13:34:49 +0800

    fix: settings chat

--- client/libs/messenger/src/lib/services/chat-settings/chat-settings.service.ts
+++ client/libs/messenger/src/lib/services/chat-settings/chat-settings.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { ChatEvent } from '../../interfaces/chat-event.interface';
 import { environment } from '@env';
 import { CacheService } from '@social/shared';
+import { UpdateChatPayload } from '../../types/chat.type';
 
 @Injectable({
   providedIn: 'root'
@@ -11,38 +12,28 @@ import { CacheService } from '@social/shared';
 export class ChatSettingsService {
   private readonly http = inject(HttpClient);
 
-  private chat$ = new BehaviorSubject<ChatEvent | null>(null);
   private readonly cacheService = inject(CacheService);
 
-  public getChat$(id: string): Observable<ChatEvent> {  
+  public getChat$(id: string): Observable<ChatEvent> {
     return this.http
       .get<ChatEvent>(environment.apiUrl + '/chats/' + id)
-      .pipe(
-        tap((response) => {
-          this.chat$.next(response);
+      .pipe(tap());
+  }
 
-          return this.chat$;
+  public updateChat$(chat: UpdateChatPayload): Observable<void> {
+    return this.http
+      .patch<void>(environment.apiUrl + '/chats/update', chat)
+      .pipe(
+        tap(() => {
+          const cache = this.cacheService.get<ChatEvent>('chat');
+
+          if (cache !== null) {
+            this.cacheService.set<ChatEvent>('chat', {
+              ...cache.value,
+              ...chat
+            });
+          }
         })
       );
-  }  
-  
-  public updateChat(chat: UpdateChatPayload): Observable<void> {
-    
-    return this.http.patch<void>(environment.apiUrl + '/chats/update', chat).pipe(
-      tap(() => {
-        const cache = this.cacheService.get<ChatEvent>('chat');
-
-        if (cache !== null) {
-          this.cacheService.set<ChatEvent>('chat', {
-            ...cache.value,
-            ...chat
-          });
-        }
-      })
-    );
   }
 }
-
-type UpdateChatPayload = Omit<ChatEvent, 'id'> & {
-  chatId?: number; 
-};
