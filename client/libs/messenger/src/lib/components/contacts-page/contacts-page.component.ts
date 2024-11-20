f643756d7e447fafb8a89514169729a6fc99b392
alex <aleksandra12mail@mail.ru>
2024-11-21 03:37:07 +0800

    feat: added adding contacts

--- client/libs/messenger/src/lib/components/contacts-page/contacts-page.component.ts
+++ client/libs/messenger/src/lib/components/contacts-page/contacts-page.component.ts
@@ -1,16 +1,32 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit
+} from '@angular/core';
 import { AsyncPipe, CommonModule, NgForOf } from '@angular/common';
 import { ContactsService } from '../../services/contacts/contacts.service';
-import { Observable, of } from 'rxjs';
+import { distinctUntilChanged, Observable, of, Subject, take } from 'rxjs';
 import { ProfileResponse } from '../../interfaces/profile-response.interface';
 import {
+  TuiAlertService,
   TuiButton,
   TuiFallbackSrcPipe,
   TuiFormatNumberPipe
 } from '@taiga-ui/core';
 import { TuiAvatar, TuiAvatarLabeled } from '@taiga-ui/kit';
 import { RouterLink } from '@angular/router';
-import { TranslocoDirective } from '@jsverse/transloco';
+import { TranslocoDirective, TranslocoService } from '@jsverse/transloco';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
+import { TuiInputModule } from '@taiga-ui/legacy';
+import { Users } from '../../interfaces/users.interface';
+import { ProfileService } from '../../services/profile/profile.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'lib-contacts-page',
@@ -25,7 +41,9 @@ import { TranslocoDirective } from '@jsverse/transloco';
     TuiFallbackSrcPipe,
     TuiButton,
     RouterLink,
-    TranslocoDirective
+    TranslocoDirective,
+    ReactiveFormsModule,
+    TuiInputModule
   ],
   templateUrl: './contacts-page.component.html',
   styleUrl: './contacts-page.component.less',
@@ -34,14 +52,58 @@ import { TranslocoDirective } from '@jsverse/transloco';
 export class ContactsPageComponent implements OnInit {
   public contacts$: Observable<ProfileResponse[]> = of([]);
   public columns: string[] = ['avatar', 'firstName', 'lastName'];
+  public listUsers: Users[] = [];
+  private readonly tuiAlertsService = inject(TuiAlertService);
+  private readonly queue$ = new Subject<Observable<unknown>>();
+  private readonly profileService = inject(ProfileService);
+  protected testForm = new FormGroup({
+    testValue: new FormControl('', Validators.required)
+  });
 
-  constructor(private readonly contactsService: ContactsService) {}
+  constructor(
+    private readonly contactsService: ContactsService,
+    private transloco: TranslocoService
+  ) {
+    this.queue$.pipe(takeUntilDestroyed()).subscribe();
+  }
 
   ngOnInit(): void {
     this.loadContacts();
+    this.contactsService
+      .getFullListUser$()
+      .subscribe((data) => (this.listUsers = data)),
+      take(1);
   }
 
   private loadContacts(): void {
     this.contacts$ = this.contactsService.getContacts$();
+  }
+
+  public addContact(): void {
+    let alertMessage = this.transloco.translate('messenger.userDoesNotExist');
+    let oops = this.transloco.translate('messenger.oops');
+    this.listUsers.forEach((user) => {
+      if (
+        this.testForm.value.testValue &&
+        (String(user.id) === this.testForm.value.testValue ||
+          String(user.phone) === this.testForm.value.testValue ||
+          String(user.customName) === this.testForm.value.testValue)
+      ) {
+        alertMessage = this.transloco.translate('messenger.addingContact');
+        oops = this.transloco.translate('messenger.hooray');
+        this.profileService.addToContact(+user.id).subscribe(() => {
+          this.loadContacts();
+        });
+
+        return;
+      }
+    });
+    this.tuiAlertsService
+      .open(alertMessage, {
+        label: oops
+      })
+      .pipe(distinctUntilChanged())
+      .subscribe(),
+      take(1);
   }
 }
--- client/libs/messenger/src/lib/services/contacts/contacts.service.ts
+++ client/libs/messenger/src/lib/services/contacts/contacts.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
 import { ProfileResponse } from '../../interfaces/profile-response.interface';
 import { environment } from '@env';
+import { Users } from '../../interfaces/users.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +11,15 @@ import { environment } from '@env';
 export class ContactsService {
   private readonly http = inject(HttpClient);
 
-
-  public getContacts$(): Observable<ProfileResponse[]> {  
+  public getContacts$(): Observable<ProfileResponse[]> {
     return this.http
       .get<ProfileResponse[]>(environment.apiUrl + '/contacts/list')
-      .pipe(
-        tap()
-      );
+      .pipe(tap());
+  }
+
+  public getFullListUser$(): Observable<Users[]> {
+    return this.http
+      .get<Users[]>(environment.apiUrl + '/users/list')
+      .pipe(tap());
   }
 }
